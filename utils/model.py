ce7476aa62614fe913273f7cf957a627a1a78c81
Girin Chutia <71626493+GirinChutia@users.noreply.github.com>
2023-07-23 21:52:45 +0530

    add checkpoint loading

--- utils/model.py
+++ utils/model.py
@@ -5,17 +5,18 @@ from torchvision.models.detection import (
     FasterRCNN_ResNet50_FPN_V2_Weights,
 )
 import warnings
-
+import torch
 warnings.filterwarnings("ignore", category=UserWarning)
 
 
-def create_model(num_classes):
+def create_model(num_classes,checkpoint=None,device='cpu'):
     """
     Create a model for object detection using the Faster R-CNN architecture.
 
     Parameters:
     - num_classes (int): The number of classes for object detection. (Total classes + 1 [for background class])
-
+    - checkpoint (str) : checkpoint path for the pretrained custom model
+    - device (str) : cpu / cuda
     Returns:
     - model (torchvision.models.detection.fasterrcnn_resnet50_fpn): The created model for object detection.
     """
@@ -23,7 +24,13 @@ def create_model(num_classes):
         pretrained=True,
         weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT,
         pretrained_backbone=True,
+        #weights_backbone = 'ResNet50_Weights.DEFAULT',
     )
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+    if checkpoint:
+        checkpoint = torch.load(checkpoint, map_location=device)
+        model.load_state_dict(checkpoint['model_state_dict'])
+    else:
+        model = model.to(device)
     return model
